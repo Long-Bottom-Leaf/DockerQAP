aef390312d67496b2a8266a9727ca4a027b60469
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-04-04 19:42:15 -0230

    built out member and tournament (and enum) class

---
+++ src/main/java/com/project/dockerapp/models/Member.java
@@ -0,0 +1,35 @@
+package com.project.dockerapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private MembershipType membershipType;
+
+    private LocalDate startDate;
+    private int duration;
+
+    @ManyToMany(mappedBy = "members", fetch = FetchType.LAZY)
+    private List<Tournament> tournaments;
+}
---
+++ src/main/java/com/project/dockerapp/models/Tournament.java
@@ -0,0 +1,33 @@
+package com.project.dockerapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "tournaments")
+public class Tournament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Double entryFee;
+    private Double cashPrize;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "member_tournament",
+            joinColumns = @JoinColumn(name = "tournament_id"),
+            inverseJoinColumns = @JoinColumn(name = "member_id")
+    )
+    private List<Member> members;
+}
