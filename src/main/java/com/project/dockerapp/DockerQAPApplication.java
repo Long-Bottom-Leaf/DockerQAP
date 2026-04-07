e03fbfe47e5d0aed30c6ffff85656f4b63009e60
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-04-06 21:33:13 -0230

    small updates to main app and yml, had trouble with spring scanning the project and creating tables

--- src/main/java/com/project/dockerapp/DockerQAPApplication.java
+++ src/main/java/com/project/dockerapp/DockerQAPApplication.java
@@ -2,10 +2,13 @@ package com.project.dockerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.project.dockerapp.models")
+@EnableJpaRepositories(basePackages = "com.project.dockerapp.repository")
 public class DockerQAPApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DockerQAPApplication.class, args);
     }
